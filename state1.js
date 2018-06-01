f1a203089f73205dcc12778472e83a775c0987fd
Vitor <vitor_48@hotmail.com>
2018-06-01 02:48:49 -0300

    new game

--- state1.js
+++
@@ -1,9 +0,0 @@
-var demo = {};
-demo.state1 = function(){};
-demo.state1.prototype = {
-    preload: function(){},
-    create: function(){
-        game.stage.backgroundColor = '#DDDDDD';
-    },
-    update: function(){}
-};
