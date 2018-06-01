f7847f15cbda1fb0a10b3406e6abf322d8337b71
Vitor <vitor@superaremedia.com.br>
2018-06-01 01:23:08 -0300

    alterações

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-var game = new Phaser.Game(600, 400, Phaser.AUTO);
+var game = new Phaser.Game(600,400, Phaser.AUTO);
+
 game.state.add('state1', demo.state1);
+game.state.add('state2', demo.state2);
 game.state.start('state1');
--- state1.js
+++ state1.js
@@ -2,6 +2,8 @@ var demo = {};
 demo.state1 = function(){};
 demo.state1.prototype = {
     preload: function(){},
-    create: function(){},
+    create: function(){
+        game.stage.backgroundColor = '#DDDDDD';
+    },
     update: function(){}
 };
